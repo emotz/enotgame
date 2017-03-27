e3cf7a7ded5426c895ed0791e8c76ce95906158c
Ozo <fizorp@gmail.com>
2017-03-27 21:04:18 +0700

    gotovo

---
+++ front_enot.js
@@ -0,0 +1,33 @@
+var insult = ['енот в коме' ,'необижен', 'обижен', 'очень обижен', 'ОБИДАЕБАНАЯ', '"ой, всё!"', 'БОМБИТ!!11!!!'];
+var personality = ['спокойный характер', 'беспокойный характер', 'жутко кипешной характер'];
+
+function numToStr(enot) {
+    var res = {};
+    res.age = enot.age;
+    res.energy = enot.energy;
+
+    if (enot.hungry == 1)
+        res.hungry = 'голоден';
+    else
+        res.hungry = 'не голоден';
+
+    res.personality = personality[enot.personality];
+    res.insult = insult[enot.insult];
+    return res;
+}
+
+function strToNum(enot) {
+    var res = {};
+    res.age = enot.age;
+    res.energy = enot.energy;
+
+    if (enot.hungry == 'Голоден')
+        res.hungry = 1;
+    else
+        res.hungry = 0;
+
+    res.personality = personality.indexOf(enot.personality);
+    res.insult = insult.indexOf(enot.insult);
+
+    return res;
+}
