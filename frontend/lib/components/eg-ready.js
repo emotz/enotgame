b6e7e66b01dbc4f118020df5fa9c3e53f5581164
Dmitry Mazurok <mazurok@outlook.com>
2017-05-11 05:22:38 +0700

    replaced tmp variable with watch

--- frontend/lib/components/eg-ready.js
+++ frontend/lib/components/eg-ready.js
@@ -3,7 +3,7 @@ import { numToStr } from '../front-enot.js';
 import time from 'game/time.js';
 export default {
     props: ['initworld'],
-    data: function () {
+    data: function() {
         return {
             comres: ['Вы купили енота'],
             ttw: 1,
@@ -11,36 +11,35 @@ export default {
         };
     },
     methods: {
-        enot_feed: function () {
-            let tmp_world = engine.lunch_feed(this.world);
-            this.comres = engine.comres(engine.compare(this.world.enot, tmp_world.enot));
-            this.world = tmp_world;
+        enot_feed: function() {
+            this.world = engine.lunch_feed(this.world);
         },
-        enot_play: function () {
-            let tmp_world = engine.lunch_play(this.world);
-            this.comres = engine.comres(engine.compare(this.world.enot, tmp_world.enot));
-            this.world = tmp_world;
+        enot_play: function() {
+            this.world = engine.lunch_play(this.world);
         },
-        enot_wait: function () {
-            let tmp_world = engine.lunch_wait(this.world, this.ttw);
+        enot_wait: function() {
+            this.world = engine.lunch_wait(this.world, this.ttw);
             this.ttw = 1;
-            this.comres = engine.comres(engine.compare(this.world.enot, tmp_world.enot));
-            this.world = tmp_world;
         },
-        ttw_control: function () {
+        ttw_control: function() {
             if (this.ttw < 1) this.ttw = 1;
         },
     },
+    watch: {
+        world(new_val, old_val) {
+            this.comres = engine.comres(engine.compare(new_val.enot, old_val.enot));
+        },
+    },
     computed: {
-        enot_to_display: function () {
-            let res = {};
-            for (let i in this.world.enot) {
+        enot_to_display: function() {
+            const res = {};
+            for (const i in this.world.enot) {
                 if (i === 'cooldown') continue;
                 res[i] = this.world.enot[i];
             }
             return numToStr(res);
         },
-        nighttime: function () {
+        nighttime: function() {
             return time.is_nighttime(this.world.environment.time);
         },
     },
